5f6918737d313301a06bb1c9a7007295eb5e5fa1
David Cabeen <mediatecnicadavidcabeen@gmail.com>
2025-02-24 18:11:33 -0500

    Se empieza a hacer el calendario

--- inicio.js
+++ inicio.js
@@ -1,12 +1,25 @@
 const container = document.querySelector(".container");
 const btnsignin = document.getElementById("btn-sign-in");
 const btnsignup = document.getElementById("btn-sign-up");
+const passwords = document.querySelectorAll(".password");
+const icons = document.querySelectorAll(".eye-icon");
 
-btnsignin.addEventListener("click",()=>{
+btnsignin.addEventListener("click", () => {
     container.classList.remove("toggle");
-
 });
-btnsignup.addEventListener("click",()=>{
+
+btnsignup.addEventListener("click", () => {
     container.classList.add("toggle");
-    
+});
+
+icons.forEach((icon, index) => {
+    icon.addEventListener("click", () => {
+        if (passwords[index].type === "password") {
+            passwords[index].type = "text";
+            icon.name = "eye-outline";
+        } else {
+            passwords[index].type = "password";
+            icon.name = "eye-off-outline";
+        }
+    });
 });
